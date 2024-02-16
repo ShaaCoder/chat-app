eaa5f7d1872466d6eaa81f1955181edda8625f2a
Shaacoder <shaacoder@gmail.com>
2024-02-16 20:29:28 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
@@ -10,6 +11,7 @@ import {app, server } from "./socket/socket.js";
 
 
 const PORT = process.env.PORT || 5000; //Ports
+const __dirname = path.resolve();
 dotenv.config(); // configuration dotenv.config
 
 app.use(express.json());
@@ -21,6 +23,12 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes) 
 app.use("/api/messages",messageRoutes) 
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
